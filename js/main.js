7f94edaa3c6536e66816a09fa77cfaa7ca11e009
jessica wray <jessica.wray@gmail.com>
2015-12-15 23:24:28 -0500

    css styling works, picked out icons and colors

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var playerTurn = 1;
-var $playerTurn = $(".player-turn");
 var $tableTarget = $(".table-target");
-var gameOver = false;
 var $square = $("");
+var colors = ["pink", "orange", "yellow", "green", "blue", "purple"];
+var icons = ["fort-awesome", "motorcycle", "lightning", "leaf", "star", "heart", "tree", "coffee", "diamond", "sun-o", "cloud", "cubes", "eye"];
 
+// This dynamically creates the board.
 function renderTableHTML(x, y){
   var table = "";
   for(j = 1; j <= x; j++){
@@ -11,7 +11,7 @@ function renderTableHTML(x, y){
     var cells = function(){
       var column = "";
       for(i = 1; i <= y; i++){
-          column = column + ("<div class='square col-xs-1 row-" + j + " column-" + i + "'><i class='fa fa-square-o fa-3x'></i></div>");
+          column = column + ("<div class='square col-xs-2 row-" + j + " column-" + i + "'><i class='fa fa-square-o fa-muted fa-5x'></i></div>");
         };
         return column;
       }();
@@ -20,7 +20,21 @@ function renderTableHTML(x, y){
     $tableTarget.html(table);
   };
 
-renderTableHTML(8,10);
+renderTableHTML(4,5);
 $square = $(".square");
+//
+// function pinkMotorcycle(){
+//   $test = $(".row-2.column-3.fa");
+//   $test.hide();
+//   $test.append("<i class='fa fa-motorcycle fa-pink'></i>");
+//
+// };
+
+// Randomly assign pairs of icons to the board
+// Can they be there all the time hidden?
+
+function takeTurn(){
+  alert("You clicked a square!");
+};
 
 $square.click(this, takeTurn);
